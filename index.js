da78b263708e477dbd4a8bdd20877a8cf6d7a246
Ubuntu <ubuntu@ip-172-31-46-114.ec2.internal>
2014-10-24 23:20:18 +0000

    Updated index.js 2

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+	var buffer = fs.readFileSync('index.html')
+  response.send(buffer.toString('utf-8'))
 })
 
 app.listen(app.get('port'), function() {
